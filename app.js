ec2a8d93bef997453d1cdf4a36b0d9648d7eaf91
ZYusuf10 <58636605+ZYusuf10@users.noreply.github.com>
2020-05-28 08:54:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -48,4 +48,8 @@ app.use(function(err, req, res, next) {
 //serve static data
 app.use(express.static('public'));
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
 module.exports = app;
